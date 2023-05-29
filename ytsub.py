d91b19b5c0968f58bf853448ebc2785da87e9508
lsw0075@gmail.com <lsw0075@gmail.com>
2023-05-29 17:55:20 +0900

    ko reorg add

--- ytsub.py
+++ ytsub.py
@@ -41,22 +41,75 @@ def process_vtt_file(file_path):
 
     return srt_file_path
 
+def process_lines(unique_lines):
+    processed_lines = []
+
+    # 문장의 끝에 대한 정규표현식을 정의합니다.
+    sentence_end_regex = r'(\w+니다|\w+니까|\w+까요|\w+다|\w+까|\w+요)(?![\w.,?\n가-힣])'
+
+    for line in unique_lines:
+        if not line.strip():
+            continue
+
+        if not processed_lines:
+            processed_lines.append(line.strip())
+        else:
+            last_line = processed_lines[-1]
+
+            # 이어지는 문장인지 확인합니다.
+            if not re.search(sentence_end_regex, last_line):
+                processed_lines[-1] += " " + line.strip()
+            else:
+                # 기존 문장에 새로운 문장이 더 있을 경우 분리
+                found_sentences = [m for m in re.finditer(sentence_end_regex, last_line)]
+
+                if len(found_sentences) > 0:  # 발견한 문장 완료 문자가 있을 경우
+                    first_sentence_end = found_sentences[0].end()
+                    processed_lines[-1] = last_line[:first_sentence_end + 1]  # end + 1을 적용
+
+                    # 완전하게 종료되지 않은 문장이 남았을 경우에만 새로운 라인에 추가합니다.
+                    if first_sentence_end + 1 < len(last_line):
+                        line = last_line[first_sentence_end + 1:] + " " + line  # end + 1을 적용
+                    else:
+                        line = line.strip()
+
+                processed_lines.append(line.strip())
+
+    # '?'와 '.' 추가해주는 조건을 처리합니다.
+    updated_lines = []
+    for line in processed_lines:
+        line = line.strip()
+        if line.endswith('까') or line.endswith('까요'):
+            if not line.endswith('까?'):
+                line += '?'
+        elif line.endswith('다') or line.endswith('요'):
+            if not line.endswith('다.') and not line.endswith('요.'):
+                line += '.'
+        updated_lines.append(line)
+    return updated_lines
+
 def vtt_to_text(vtt_filename, output_filename):
     captions = webvtt.read(vtt_filename)
     unique_lines = []
 
     for caption in captions:
         line = caption.text.strip()
-        num_of_lines = len(line.split('\n'))  # 줄 개수 확인
+        # num_of_lines = len(line.split('\n'))  # 줄 개수 확인
 
-        if num_of_lines <= 1: 
+        # if num_of_lines <= 1: 
+        #     # 중복 line 검사를 위해 unique_lines의 마지막 3개 요소를 검사
+        #     recent_lines = unique_lines[-3:]
+        #     if line not in recent_lines:
+        #         unique_lines.append(line)
+        lines = caption.text.strip().split('\n')
+        for line in lines:    
             # 중복 line 검사를 위해 unique_lines의 마지막 3개 요소를 검사
             recent_lines = unique_lines[-3:]
             if line not in recent_lines:
                 unique_lines.append(line)
-
+    
     with open(output_filename, 'w', encoding='utf-8') as output_file:
-        for line in unique_lines:
+        for line in process_lines(unique_lines):
             output_file.write(line + '\n')
             
 def download_auto_subtitles(video_url):
